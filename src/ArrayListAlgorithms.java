8c4e55763b58e8097227450ff284d4da86b0f587
Jian Huang <jianh2@Nycstudents.net>
2023-02-08 09:28:15 -0500

    2/8

--- src/ArrayListAlgorithms.java
+++ src/ArrayListAlgorithms.java
@@ -1,3 +1,4 @@
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Locale;
@@ -38,28 +39,113 @@ public class ArrayListAlgorithms {
 //    }
 //    return count;
 //    }
-    public static void replaceWithCaps(ArrayList<String> wordList)
-    {for (int i = 0; i< wordList.size(); i++){
-    if (wordList.get(i).substring(wordList.get(i).length()-1).equals("s")){
-        wordList.set(i, wordList.get(i).toUpperCase());
-    }
-    }
+//    public static void replaceWithCaps(ArrayList<String> wordList)
+//    {for (int i = 0; i< wordList.size(); i++){
+//    if (wordList.get(i).substring(wordList.get(i).length()-1).equals("s")){
+//        wordList.set(i, wordList.get(i).toUpperCase());
+//    }
+//    }
 
-}
-    public static int indexOfMinimum(ArrayList<Integer> intList) {
-    int minimum = intList.get(0);
-    for (int i = 0; i < intList.size(); i++){
-        if (intList.get(i) < minimum){
-            minimum = intList.get(i);
+
+//    public static int indexOfMinimum(ArrayList<Integer> intList) {
+//    int minimum = intList.get(0);
+//    for (int i = 0; i < intList.size(); i++){
+//        if (intList.get(i) < minimum){
+//            minimum = intList.get(i);
+//        }
+//    }
+//
+//    for (int i = 0; i < intList.size(); i++){
+//        if (intList.get(i) == minimum){
+//            return i;
+//        }
+//    }
+//    return 1;
+//    }
+//    public static boolean areIdentical(ArrayList<Integer> numList1, ArrayList<Integer> numList2)
+//    {
+//       for (int i = 0; i < numList1.size(); i++){
+//           if (numList1.get(i) != numList2.get(i)){
+//               return false;
+//           }
+//       }
+//       return true;
+//    }
+
+//    public static void removeOdds(ArrayList<Integer> numList) {
+//
+//        {
+//            for (int i = 0; i < numList.size(); i++) {
+//                if (numList.get(i) % 2 > 0) {
+//                    numList.remove(i);
+//                    i--;
+//                }
+//            }
+//        }
+//    }
+//    public static void wackyVowels(ArrayList<String> wordList)
+//    {
+//    for (int i = 0; i < wordList.size(); i++){
+//        if (wordList.get(i).contains("a") || wordList.get(i).contains("e") ||  wordList.get(i).contains("i") ||  wordList.get(i).contains("o")){
+//            wordList.remove(i);
+//            i--;
+//        }else{
+//            wordList.add(i,wordList.get(i));
+//            i++;
+//        }
+//    }
+//
+//    }
+
+//    public static void duplicateUpperAfter(ArrayList<String> wordList)
+//    {for (int i = 0; i < wordList.size(); i++){
+//    wordList.add(wordList.get(i).toUpperCase());
+//    i++;
+//    }
+//    }
+//    public static ArrayList<String> parseWordsAndReverse(String sentence) {
+//        ArrayList<String> arr = new ArrayList<String>();
+//        int count = 0;
+//        if (sentence.indexOf(" ") < 0){
+//            arr.add(sentence);
+//            return arr;
+//        }
+//
+//        String[] words = sentence.split(" ");
+//        words.asList();
+//
+//            for (int i = words.length-1; i <= 0; i--){
+//                arr.add(count,words[i]);
+//                count++;
+//            }
+//
+//
+//        return arr;
+//
+//
+//
+//
+//
+//    }
+    public static ArrayList<Integer> modes(int[] numList)
+    {
+        int mode = numList[0];
+        int maxMode = 0;
+        int count  = 0;
+        int maxCount = 0;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < numList.length; i++){
+            if (numList[i] == mode){
+                count++;
+                maxCount++;
+                list.add(i)
+            }else{
+                if (count > maxCount)
+                maxMode = mode;
+                mode = numList[i];
+            }
+
         }
-    }
 
-    for (int i = 0; i < intList.size(); i++){
-        if (intList.get(i) == minimum){
-            return i;
-        }
     }
-    return 1;
-    }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -27,32 +27,32 @@ public class Main {
 
 //        System.out.println("AFTER: " + names);
 
-        ArrayList<String> names = new ArrayList<String>();
-        names.add("Jesse");
-        names.add("Bart");
-        names.add("David");
-        names.add("Abigail");
-        names.add("Frank");
-        names.add("Steph");
-        names.add("Thomas");
-        names.add("Jimmy");
-        names.add("Joe");
-        names.add("Klein");
-        names.add("Daisy");
-
-        System.out.println("BEFORE: " + names);
-
-        // write a loop below to traverse names and REMOVE each name that
-        // is exactly 5 letters
-
-        for (int i = 0; i < names.size(); i++){
-            if (names.get(i).length() == 5){
-                names.remove(i);
-                i--;
-            }
-        }
-
-        System.out.println("AFTER: " + names);
+//        ArrayList<String> names = new ArrayList<String>();
+//        names.add("Jesse");
+//        names.add("Bart");
+//        names.add("David");
+//        names.add("Abigail");
+//        names.add("Frank");
+//        names.add("Steph");
+//        names.add("Thomas");
+//        names.add("Jimmy");
+//        names.add("Joe");
+//        names.add("Klein");
+//        names.add("Daisy");
+//
+//        System.out.println("BEFORE: " + names);
+//
+//        // write a loop below to traverse names and REMOVE each name that
+//        // is exactly 5 letters
+//
+//        for (int i = 0; i < names.size(); i++){
+//            if (names.get(i).length() == 5){
+//                names.remove(i);
+//                i--;
+//            }
+//        }
+//
+//        System.out.println("AFTER: " + names);
 
 //        ArrayList<String> stringList = new ArrayList<String>(Arrays.asList("this", "list", "contains", "several", "words"));
 //        boolean contains = ArrayListAlgorithms.containsTarget(stringList, "is");
@@ -81,18 +81,50 @@ public class Main {
 //        ArrayListAlgorithms.replaceWithCaps(stringList12);
 //        System.out.println(stringList12);
 
-        ArrayList<String> stringList13 = new ArrayList<String>(Arrays.asList("yeah", "pat", "but", "Met", "Yankee"));
-        ArrayListAlgorithms.replaceWithCaps(stringList13);
-        System.out.println(stringList13);
-        ArrayList<Integer> intList4 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
-        int index = ArrayListAlgorithms.indexOfMinimum(intList4);
-        System.out.println(index);
-        ArrayList<Integer> intList5 = new ArrayList<Integer>(Arrays.asList(6, 8, 3, 5, 6, 3, 2, 3, 2));
-        index = ArrayListAlgorithms.indexOfMinimum(intList5);
-        System.out.println(index);
-        ArrayList<Integer> intList6 = new ArrayList<Integer>(Arrays.asList(10, 12, 9, 9, 8));
-        index = ArrayListAlgorithms.indexOfMinimum(intList6);
-        System.out.println(index);
+
+//        ArrayList<Integer> intList4 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
+//        int index = ArrayListAlgorithms.indexOfMinimum(intList4);
+//        System.out.println(index);
+//        ArrayList<Integer> intList5 = new ArrayList<Integer>(Arrays.asList(6, 8, 3, 5, 6, 3, 2, 3, 2));
+//        index = ArrayListAlgorithms.indexOfMinimum(intList5);
+//        System.out.println(index);
+//        ArrayList<Integer> intList6 = new ArrayList<Integer>(Arrays.asList(10, 12, 9, 9, 8));
+//        index = ArrayListAlgorithms.indexOfMinimum(intList6);
+//        System.out.println(index);
+
+
+        int[] intList18 = {1, 2, 3, 2, 4, 5, 5, 6};
+        ArrayList<Integer> modes = ArrayListAlgorithms.modes(intList18);
+        System.out.println(modes);
+
+        int[] intList19 = {1, 2, 6, 2, 3, 4, 6, 5, 5, 6, 7};
+        ArrayList<Integer> modes2 = ArrayListAlgorithms.modes(intList19);
+        System.out.println(modes2);
+
+        int[] intList20 = {1, 2, 1, 2, 4, 1, 2, 4, 1, 1, 3};
+        ArrayList<Integer> modes3 = ArrayListAlgorithms.modes(intList20);
+        System.out.println(modes3);
+
+        int[] intList21 = {1, 2, 3, 4, 3, 2, 1, 4, 1, 2, 3};
+        ArrayList<Integer> modes4 = ArrayListAlgorithms.modes(intList21);
+        System.out.println(modes4);
+
+        // all numbers appear 1 time
+        int[] intList22 = {1, 2, 3, 4, 5, 6};
+        ArrayList<Integer> modes5 = ArrayListAlgorithms.modes(intList22);
+        System.out.println(modes5);
+
+        // all numbers appear 3 times
+        int[] intList23 = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 1, 2, 3, 4, 5, 6};
+        ArrayList<Integer> modes6 = ArrayListAlgorithms.modes(intList23);
+        System.out.println(modes6);
+
+        // this one has an extra 7, so 1, 2, 3, 4, 5, and 6 are all modes!
+        int[] intList24 = {1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6};
+        ArrayList<Integer> modes7 = ArrayListAlgorithms.modes(intList24);
+        System.out.println(modes7);
+;
+
 
     }
 }
